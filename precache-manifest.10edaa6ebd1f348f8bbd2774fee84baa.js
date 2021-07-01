0be4f20400284ae3c8376a09348d6fe78e6c8a66
Tanuj <tanuj.kimadi1@gmail.com>
2021-07-01 17:18:11 +0530

    Updates

---
+++ precache-manifest.10edaa6ebd1f348f8bbd2774fee84baa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d305825e10189d7e90a46e97b02bc605",
+    "url": "/.github.io/React-Website/index.html"
+  },
+  {
+    "revision": "580c6963af9dc8f5071c",
+    "url": "/.github.io/React-Website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f9a888c64dba39089d44",
+    "url": "/.github.io/React-Website/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "580c6963af9dc8f5071c",
+    "url": "/.github.io/React-Website/static/js/2.04ed21d9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/.github.io/React-Website/static/js/2.04ed21d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a888c64dba39089d44",
+    "url": "/.github.io/React-Website/static/js/main.6a5a0d72.chunk.js"
+  },
+  {
+    "revision": "b9d6969ba83463985a5e",
+    "url": "/.github.io/React-Website/static/js/runtime-main.d03dc0ee.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/.github.io/React-Website/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/.github.io/React-Website/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/.github.io/React-Website/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/.github.io/React-Website/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/.github.io/React-Website/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/.github.io/React-Website/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/.github.io/React-Website/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/.github.io/React-Website/static/media/software.43049cae.jpeg"
+  }
+]);
